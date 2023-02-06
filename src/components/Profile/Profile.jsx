3bf982b6917f373f671d0d0858e92e276208bd30
Alexandr Katyshev <katyshev1@gmail.com>
2023-02-06 14:58:06 +0200

    added track to some files

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,61 @@
+import PropTypes from "prop-types";
+import {
+  ProfileCard,
+  Description,
+  Avatar,
+  Name,
+  Tag,
+  Location,
+  Stats,
+  StatsItem,
+  Label,
+  Quantity,
+} from "./Profile.styled";
+
+export const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) => (
+  <ProfileCard>
+    <Description>
+      <Avatar
+        src={avatar}
+        alt={username}
+        width="120"
+      />
+      <Name>{username}</Name>
+      <Tag>@{tag}</Tag>
+      <Location>{location}</Location>
+    </Description>
+
+    <Stats>
+      <StatsItem>
+        <Label>Followers</Label>
+        <Quantity>{followers}</Quantity>
+      </StatsItem>
+      <StatsItem>
+        <Label>Views</Label>
+        <Quantity>{views}</Quantity>
+      </StatsItem>
+      <StatsItem>
+        <Label>Likes</Label>
+        <Quantity>{likes}</Quantity>
+      </StatsItem>
+    </Stats>
+  </ProfileCard>
+);
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.shape({
+    followers: PropTypes.number,
+    views: PropTypes.number,
+    likes: PropTypes.number,
+  }),
+};
---
+++ src/components/Profile/Profile.styled.jsx
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+export const ProfileCard = styled.div`
+    margin: 0 auto;
+    width: 300px;
+    border: 1px solid lightgray;
+    border-radius: 4px;
+    overflow: hidden;
+    box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+`;
+
+export const Description = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 40px;
+`;
+
+export const Avatar = styled.img`
+    width: 120px;
+    margin-bottom: 20px;
+`;
+
+export const Name = styled.p`
+    margin-bottom: 10px;
+    font-size: 21px;
+    font-weight: 700;
+`;
+
+export const Tag = styled.p`
+    margin-bottom: 10px;
+    font-weight: 500;
+    color: darkgray;
+`;
+
+export const Location = styled.p`
+    font-weight: 500;
+    color: darkgray;
+`;
+
+export const Stats = styled.ul`
+    display: flex;
+`;
+
+export const StatsItem = styled.li`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: calc(100% / 3);
+    padding: 20px;
+    background-color: #f3f6f9;
+    border-top: 1px solid lightgray;
+
+    :not(:last-child) {
+        border-right: 1px solid lightgray;
+    }
+`;
+
+export const Label = styled.span`
+    margin-bottom: 5px;
+    font-weight: 500;
+    color: darkgray;
+`;
+
+export const Quantity = styled.span`
+    font-weight: 700;
+`;
